e7f1d4bca9b0595e23aedebe26fcf3c629f079e0
caatnasc <caatnasc@gmail.com>
2025-05-04 22:49:59 +0000

    EXEMPLO 5

--- main.js
+++ main.js
@@ -32,3 +32,18 @@ function multiplicacao(numero1, numero2){
 let multiplicacao1 = multiplicacao(6, 7)
 let multiplicacao2 = multiplicacao(9, 10)
 let multiplicacao3 = multiplicacao(12, 25)
+
+console.log(multiplicacao1)
+console.log(multiplicacao2)
+console.log(multiplicacao3)
+
+//EXEMPLO 5
+function parOuImpar(numero){
+    if(numero % 2 === 0){
+        return "PAR"
+    }
+    return "ÍMPAR"
+    
+}
+
+console.log(parOuImpar(14))
