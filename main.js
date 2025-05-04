11c9f079492fd44242d2dba416c6af8355e8bbd5
caatnasc <caatnasc@gmail.com>
2025-05-04 22:44:29 +0000

    EXEMPLO 3

--- main.js
+++ main.js
@@ -12,3 +12,14 @@ function somar(valorA, valorB){
 }
 
 somar(20, 12)
+
+//EXEMPLO 3
+function mensagemBoasVindas(nome, cargo){
+    alert('Seja bem-vindo(a) ${nome} ${cargo}')
+}
+
+let nome = prompt("Digite o nome do funcionário:")
+let cargo = prompt("Digite o cargo do funcionário:")
+
+alert(mensagemBoasVindas(nome, cargo))
+console.log(mensagemBoasVindas(nome, cargo))
